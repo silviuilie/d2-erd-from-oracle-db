ee2837b02136fee8ce72df8f1ac305e71c77228e
silviu <silviu>
2024-08-04 13:20:22 +0200

    add --excludes argument

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const path = require('node:path');
 const fs = require("fs");
 const prompt = require('prompt');
 const {Eta} = require("eta");
+
 const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+
 const {execFileSync: sh} = require("child_process");
 
 prompt.message = "oracle";
 prompt.start();
 
 
-async function render(dbUrl) {
+async function render(dbUrl, otherOptions) {
     oracle.fetchAsString = [oracle.CLOB];
 
     prompt.get(
@@ -26,8 +29,13 @@ async function render(dbUrl) {
                 connectString: dbUrl
             });
 
+            const template = new Eta({views: path.join(__dirname, "templates")});
+
+            console.log('render:->',template.render("sql.default.eta", otherOptions))
+
             const dbResult = await connection.execute(
-                fs.readFileSync("./playground/default.sql").toString(),
+                template.render("sql.default.eta", otherOptions),
+                // fs.readFileSync("./templates/sql.default.eta").toString(),
                 [], // A bind parameter is needed to disambiguate the following options parameter and avoid ORA-01036
                 {
                     outFormat: 4002,     // outFormat can be OBJECT or ARRAY.  The default is ARRAY
@@ -36,24 +44,28 @@ async function render(dbUrl) {
                 }
             );
             const rs = dbResult.rows;
-            const template = new Eta({views: path.join(__dirname, "templates")});
-            const output = template.render("default.eta", rs);
+            const output = template.render("d2.default.eta", rs);
             fs.writeFileSync("output.d2", output);
             console.log('dagre..')
             sh("d2", ["--layout=dagre", "output.d2", "out-dagre.svg"]);
-            console.log('tala..')
-            sh("d2", ["--layout=tala", "output.d2", "out-tala.svg"]);
+            // console.log('tala..')
+            // sh("d2", ["--layout=tala", "output.d2", "out-tala.svg"]);
             // return output;
         });
 
 }
 
 async function main() {
-
-    const input = yargs(process.argv.slice(2)).parse();
-    console.log('input',input)
-    console.log('input.d2',input.d2)
-    const schema = render(process.argv[2]);
+    var otherOptions = require('yargs/yargs')(process.argv.slice(2))
+        .option('excludes', {
+            alias: 'e',
+            describe: 'comma separated list of tables to exclude'
+        })
+        .coerce('excludes', ex=>{ return ex.split(/\s*,\s*/).map(table=>"\'"+table+"\'")})
+        .parse()
+    console.log(otherOptions)
+    const url = process.argv[2];
+    const schema = render(url, otherOptions);
     return;
 }
 
